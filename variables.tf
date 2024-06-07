932ac61b68a624319460198204b043facc3f49c3
João Lacerda <joao.sclf@gmail.com>
2024-06-07 10:31:03 -0300

    add variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,7 @@ variable "location" {
   description = "Região onde os recursos serão criados na Azure"
   type = string
 }
+
+variable "account_tier" {
+  description = "Tier de storage Account na Azure"
+}
