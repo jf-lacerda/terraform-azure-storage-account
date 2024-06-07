d6d1d03d9ecb0df4947be08ba69b27d1aa19521a
João Lacerda <joao.sclf@gmail.com>
2024-06-07 16:53:49 -0300

    local values

--- storege_account.tf
+++ storege_account.tf
@@ -1,21 +1,21 @@
 resource "azurerm_resource_group" "resource_group" {
   name     = ""
-  location = ""
+  location = var.location
 
   tags = ""
 }
 
 resource "azurerm_storage_account" "storage_account" {
   name                     = ""
-  resource_group_name      = ""
-  location                 = ""
-  account_tier             = ""
-  account_replication_type = ""
+  resource_group_name      = azurerm_resource_group.resource_group.nome
+  location                 = var.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
 
   tags = ""  
 }
 
 resource "azurerm_storage_container" "container" {
   name                  = ""
-  storage_account_name  = ""
+  storage_account_name  = azurerm_storage_account.storage_account.name
 }
--- variables.tf
+++ variables.tf
@@ -5,4 +5,12 @@ variable "location" {
 
 variable "account_tier" {
   description = "Tier de storage Account na Azure"
+  type = string
+  default = "Standard"
+}
+
+variable "account_replication_type" {
+  description = "Tipo de replicação de dados da Storage Account"
+  type = string
+  default = "LRS"
 }
