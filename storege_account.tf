c079cbca8b4bf6a9993a6c9e437d05a6c707f18a
João Lacerda <joao.sclf@gmail.com>
2024-06-10 09:31:44 -0300

    add file

--- storege_account.tf
+++ storege_account.tf
@@ -7,19 +7,16 @@ resource "azurerm_resource_group" "resource_group" {
 
 resource "azurerm_storage_account" "storage_account" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.resource_group.nome
+  resource_group_name      = azurerm_resource_group.resource_group.name
   location                 = var.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
 
-  tags = local.common_tags  
+  tags = local.common_tags
 }
 
 resource "azurerm_storage_container" "container" {
-  name                  = var.container_name
-  storage_account_name  = azurerm_storage_account.storage_account.name
+  name                 = var.container_name
+  storage_account_name = azurerm_storage_account.storage_account.name
 }
 
-resource "" "name" {
-  
-}
--- variables.tf
+++ variables.tf
@@ -1,34 +1,34 @@
 variable "location" {
   description = "Região onde os recursos serão criados na Azure"
-  type = string
+  type        = string
 }
 
 variable "account_tier" {
   description = "Tier de storage Account na Azure"
-  type = string
-  default = "Standard"
+  type        = string
+  default     = "Standard"
 }
 
 variable "account_replication_type" {
   description = "Tipo de replicação de dados da Storage Account"
-  type = string
-  default = "LRS"
+  type        = string
+  default     = "LRS"
 }
 
 variable "resource_group_name" {
   description = "Nome para Resource Group na Azure"
-  type = string
-  default = "rg-curso-terraform"
+  type        = string
+  default     = "rg-curso-terraform"
 }
 
 variable "storage_account_name" {
   description = "Nome da Storage Account na Azure"
-  type = string
-  default = "joaolacerda-terraform"
+  type        = string
+  default     = "joaolacerda-terraform"
 }
 
 variable "container_name" {
-  description = "Nome do Container na Azure"  
-  type = string
-  default = "container-terraform"
+  description = "Nome do Container na Azure"
+  type        = string
+  default     = "container-terraform"
 }
